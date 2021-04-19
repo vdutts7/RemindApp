eb863d51bfb100129b8ce1e9223b8f88cdfbef58
Vivek Dutta <vivekd@berkeley.edu>
2021-04-19 16:20:10 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 // ./App.js
 
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import MainStackNavigator from "./NavigationFiles/StackNavigator"
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import MainStackNavigator from "./NavigationFiles/StackNavigator"
+// import LoginPageNav from "./NavigationFiles/mainNav";
 
-import LoginPageNav from "./NavigationFiles/mainNav";
+//  const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <LoginPageNav/>
+//     </NavigationContainer>
+//   );
+// }
+// export default App;
 
 
+import React from 'react';
 
- const App = () => {
+import { 
+  NavigationContainer
+} from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import MainTabScreen from './Screens/MainTabScreen';
+import SupportScreen from './Screens/SupportScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+
+
+import { DrawerContent } from './Screens/DrawerContent';
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  // const [isLoading, setIsLoading] = React.useState(true);
+  // const [userToken, setUserToken] = React.useState(null); 
   return (
     <NavigationContainer>
-      <LoginPageNav/>
+        <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+          <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+          <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+          <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
 export default App;
